186ebdbcccfb13b6d43006b86e568be9fa47ed1c
alifiarhafizh8-collab <alifiarhafizh8@gmail.com>
2026-07-25 09:59:34 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,17 +1,40 @@
+const canvas = document.getElementById("gameCanvas");
+const ctx = canvas.getContext("2d");
+
+const hpText = document.getElementById("hp");
+const scoreText = document.getElementById("score");
+
 const playerImg = new Image();
 playerImg.src = "assets/player.png";
 
 const zombieImg = new Image();
 zombieImg.src = "assets/zombie.png";
 
-const treeImg = new Image();
-treeImg.src = "assets/tree.png";
-
-const medkitImg = new Image();
-medkitImg.src = "assets/medkit.png";
+const backgroundImg = new Image();
+backgroundImg.src = "assets/background.png";
 
 const bulletImg = new Image();
 bulletImg.src = "assets/bullet.png";
 
-const bgImg = new Image();
-bgImg.src = "assets/background.png";
+const medkitImg = new Image();
+medkitImg.src = "assets/medkit.png";
+
+const player = {
+    x: 160,
+    y: 520,
+    width: 40,
+    height: 40,
+    speed: 10,
+    hp: 100
+};
+
+let score = 0;
+
+const bullets = [];
+const zombies = [];
+
+const left = document.getElementById("left");
+const right = document.getElementById("right");
+const up = document.getElementById("up");
+const down = document.getElementById("down");
+const attack = document.getElementById("attack");
