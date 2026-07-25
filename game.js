3c6404ad9d719547991495c0b5962a733caeed9f
alifiarhafizh8-collab <alifiarhafizh8@gmail.com>
2026-07-25 09:56:43 +0700

    Update game.js

--- game.js
+++ game.js
@@ -1,1 +1,17 @@
+const playerImg = new Image();
+playerImg.src = "assets/player.png";
 
+const zombieImg = new Image();
+zombieImg.src = "assets/zombie.png";
+
+const treeImg = new Image();
+treeImg.src = "assets/tree.png";
+
+const medkitImg = new Image();
+medkitImg.src = "assets/medkit.png";
+
+const bulletImg = new Image();
+bulletImg.src = "assets/bullet.png";
+
+const bgImg = new Image();
+bgImg.src = "assets/background.png";
